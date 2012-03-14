52b5579987ad9af382799b5ea83d2bbe788122f2
Simeon Bird <spb41@ias.edu>
2012-03-14 15:02:58 -0400

    Only load gas or nHI, not both and clf in an attempt to save memory

--- make_dla_plots.py
+++ make_dla_plots.py
@@ -24,63 +24,69 @@ for (base,snapnum) in [(bb,ss) for bb in bases for ss in snaps]:
     outdir=path.join(base,"plots")
     print "Saving plots for snapshot ",snapnum," to ",outdir
 
-    hplots=dp.HaloHIPlots(base,snapnum,minpart=minpart)
-    #Fig 6
-    plt.figure()
-    hplots.ahalo.plot_pretty_halo()
-    save_figure(path.join(outdir,"Arepo_"+str(snapnum)+"pretty_halo"))
+    #Load only the gas grids
+    hplots=dp.HaloHIPlots(base,snapnum,minpart=minpart,skip_grid=1)
     plt.figure()
     hplots.ahalo.plot_pretty_gas_halo()
     save_figure(path.join(outdir,"Arepo_"+str(snapnum)+"pretty_gas_halo"))
-
-    plt.figure()
-    hplots.ghalo.plot_pretty_halo()
-    save_figure(path.join(outdir,"Gadget_"+str(snapnum)+"pretty_halo"))
-    plt.figure()
+    plt.clf()
     hplots.ghalo.plot_pretty_gas_halo()
     save_figure(path.join(outdir,"Gadget_"+str(snapnum)+"pretty_gas_halo"))
-
     #Radial profiles
-    plt.figure()
+    plt.clf()
+    hplots.plot_radial_profile(0)
+
+    del hplots
+    #Load only the nHI grids
+    hplots=dp.HaloHIPlots(base,snapnum,minpart=minpart,skip_grid=2)
     hplots.plot_radial_profile(0)
     save_figure(path.join(outdir,"radial_profile_halo_0_"+str(snapnum)))
 
+
+    #Fig 6
+    plt.clf()
+    hplots.ahalo.plot_pretty_halo()
+    save_figure(path.join(outdir,"Arepo_"+str(snapnum)+"pretty_halo"))
+    plt.clf()
+    hplots.ghalo.plot_pretty_halo()
+    save_figure(path.join(outdir,"Gadget_"+str(snapnum)+"pretty_halo"))
+
     #Fig 9
-    plt.figure()
+    plt.clf()
     dp.plot_totalHI(base,snapnum)
     save_figure(path.join(outdir,"total_HI_"+str(snapnum)))
 
     #Fig 10
-    plt.figure()
+    plt.clf()
     hplots.plot_sigma_DLA()
     save_figure(path.join(outdir,"sigma_DLA_"+str(snapnum)))
 
     #Fig 10
-    plt.figure()
+    plt.clf()
     hplots.plot_sigma_DLA(17)
     save_figure(path.join(outdir,"sigma_DLA_17_"+str(snapnum)))
 
     #Fig 10
-    plt.figure()
+    plt.clf()
     hplots.plot_rel_sigma_DLA()
     save_figure(path.join(outdir,"rel_sigma_DLA_"+str(snapnum)))
 
 #    #Fig 11
-#    plt.figure()
+#    plt.clf()
 #    hplots.plot_dN_dla()
 #    save_figure(path.join(outdir,"dNdz_"+str(snapnum)))
 
     #Fig 12
-    plt.figure()
+    plt.clf()
     hplots.plot_column_density()
     save_figure(path.join(outdir,"columden_"+str(snapnum)))
 
     #Fig 12
-    plt.figure()
+    plt.clf()
     hplots.plot_rel_column_density()
     save_figure(path.join(outdir,"columden_rel_"+str(snapnum)))
 
 #     #Fig 5
-#     plt.figure()
+#     plt.clf()
 #     hplots.plot_halo_mass_func()
 #     save_figure(path.join(outdir,"halo_func_"+str(snapnum)))
