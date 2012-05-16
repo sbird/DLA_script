64f3893cbc38ee5242408803cc9d6862c913b567
Simeon Bird <spb41@ias.edu>
2012-05-15 23:15:27 -0400

    Fix column den

--- halohi.py
+++ halohi.py
@@ -605,18 +605,22 @@ class HaloHI:
         Returns:
             (NHI, f_N_table) - N_HI (binned in log) and corresponding f(N)
         """
-        NHI_table = np.logspace(minN, maxN,(maxN-minN)/dlogN,endpoint=True)
+        NHI_table = np.log10(np.logspace(minN, maxN,(maxN-minN)/dlogN,endpoint=True))
         #Grid size (in cm^2)
         dX=self.absorption_distance()
-        flat_grid=np.ravel(self.sub_nHI_grid)
-        #Ouch memory
-        cell_areas = np.ravel(self.ngrid*np.ones(np.shape(self.sub_nHI_grid)))
-        (f_N,NHI_table)=np.histogram(flat_grid,np.log10(NHI_table),weights=cell_areas)
+        flat_grid=np.ravel(self.sub_nHI_grid[0])
+        (tot_f_N,NHI_table)=np.histogram(flat_grid,NHI_table)
+        tot_f_N *= 2.*self.sub_radii[0]/self.ngrid[0]
+        for ii in xrange(1,self.nhalo):
+            flat_grid=np.ravel(self.sub_nHI_grid[ii])
+            (f_N,NHI_table_in)=np.histogram(flat_grid,NHI_table)
+            f_N *= 2.*self.sub_radii[ii]/self.ngrid[ii]
+            tot_f_N+=f_N
         NHI_table=(10.)**NHI_table
         #To compensate for any rounding
         dlogN_real = (np.log10(NHI_table[-1])-np.log10(NHI_table[0]))/(np.size(NHI_table)-1)
-        f_N=(1.*f_N)/((NHI_table[0:-1]*(10.)**dlogN_real-NHI_table[0:-1])*dX)
-        return (NHI_table[0:-1]*(10.)**(dlogN_real/2.), f_N)
+        tot_f_N=(tot_f_N)/((NHI_table[0:-1]*(10.)**dlogN_real-NHI_table[0:-1])*dX)
+        return (NHI_table[0:-1]*(10.)**(dlogN_real/2.), tot_f_N)
 
     def omega_DLA(self, maxN):
         """Compute Omega_DLA, defined as:
