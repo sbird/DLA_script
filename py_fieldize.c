8ff15fe0df93247c8954714baca2a6905fce4911
Simeon Bird <spb@ias.edu>
2013-01-30 18:36:37 -0500

    Use slightly more accurate kernel integration

--- py_fieldize.c
+++ py_fieldize.c
@@ -24,6 +24,43 @@ void add_to_data_array(double * result, int * id1, int * id2, PyArrayObject * fi
             (*il)++;
 }
 
+/*Compute the SPH weighting for this cell, using the trapezium rule.
+ * rr is the smoothing length, r0 is the distance of the cell from the center*/
+double compute_sph_cell_weight(double rr, double r0)
+{
+    double total=0;
+    double h2 = rr*rr;
+    //Do the z integration with the trapezium rule.
+    //Evaluate this at some fixed (well-chosen) abcissae
+    double zc=0;
+    if(rr/2 > r0)
+        zc=sqrt(h2/4-r0*r0);
+    double zm = sqrt(h2-r0*r0);
+    double zz[5]={zc,(3*zc+zm)/4.,(zc+zm)/2.,(zc+3*zm)/2,zm};
+    double kern[5];
+    double kfac= 8/M_PI/pow(rr,3);
+    for(int i=0; i< 5;i++){
+        kern[i] = 2*pow(1-sqrt(zz[i]*zz[i]+r0*r0)/rr,3)*kfac;
+    }
+    for(int i=0; i< 4;i++){
+        //Trapezium rule. Factor of 1/2 goes away because we double the integral
+        total +=(zz[i+1]-zz[i])*(kern[i+1]+kern[i]);
+    }
+    if(rr/2 > r0){
+        double zz2[9]={0,zc/16.,zc/8.,zc/4.,3*zc/8,zc/2.,5/8.*zc,3*zc/4.,zc};
+        double kern2[9];
+        for(int i=0; i< 9;i++){
+            double R = sqrt(zz2[i]*zz2[i]+r0*r0);
+            kern2[i] = kfac*(1-6*(R/rr)*R/rr+6*pow(R/rr,3));
+        }
+        for(int i=0; i< 8;i++){
+            //Trapezium rule. Factor of 1/2 goes away because we double the integral
+            total +=(zz2[i+1]-zz2[i])*(kern2[i+1]+kern2[i]);
+        }
+    }
+    return total;
+}
+
 //  int    3*nval arr  nval arr  nval arr   nx*nx arr  int     nval arr  (or 0)
 //['nval','pos',     'radii',    'value',   'field',   'nx',    'weights']
 PyObject * Py_SPH_Fieldize(PyObject *self, PyObject *args)
@@ -54,12 +91,11 @@ PyObject * Py_SPH_Fieldize(PyObject *self, PyObject *args)
             if (weight == 0)
                 weight = 1;
         }
-        //99% of the kernel is inside 0.85 of the smoothing length.
-        //Neglect the rest.
-        int upgx = floor(pp[0]+0.85*rr);
-        int upgy = floor(pp[1]+0.85*rr);
-        int lowgx = floor(pp[0]-0.85*rr);
-        int lowgy = floor(pp[1]-0.85*rr);
+        //Max size of kernel
+        int upgx = floor(pp[0]+rr);
+        int upgy = floor(pp[1]+rr);
+        int lowgx = floor(pp[0]-rr);
+        int lowgy = floor(pp[1]-rr);
         //Try to save some integrations if this particle is totally in this cell
         if (lowgx==upgx && lowgy==upgy && lowgx >= 0 && lowgy >= 0){
                 *((double *) PyArray_GETPTR2(field,lowgx,lowgy))+=val/weight;
@@ -75,46 +111,29 @@ PyObject * Py_SPH_Fieldize(PyObject *self, PyObject *args)
                 lowgx=0;
             if(lowgy < 0)
                 lowgy=0;
+            /*Array for storing cell weights*/
+            double sph_w[upgy-lowgy+1][upgx-lowgx+1];
+            /*Total of cell weights*/
+            double total=0;
+            /*First compute the cell weights*/
+            #pragma omp parallel for reduction(+:total)
             for(int gy=lowgy;gy<=upgy;gy++)
                 for(int gx=lowgx;gx<=upgx;gx++){
-                    double total=0;
                     double xx = gx-pp[0]+0.5;
                     double yy = gy-pp[1]+0.5;
                     double r0 = sqrt(xx*xx+yy*yy);
-                    if(r0 > rr)
+                    if(r0 > rr){
+                        sph_w[gy-lowgy][gx-lowgx]=0;
                         continue;
-                    double h2 = rr*rr;
-                    //Do the z integration with the trapezium rule.
-                    //Evaluate this at some fixed (well-chosen) abcissae
-                    double zc=0;
-                    if(rr/2 > r0)
-                        zc=sqrt(h2/4-r0*r0);
-                    double zm = sqrt(h2-r0*r0);
-                    double zz[5]={zc,(3*zc+zm)/4.,(zc+zm)/2.,(zc+3*zm)/2,zm};
-                    double kern[5];
-                    double kfac= 8/M_PI/pow(rr,3);
-                    for(int i=0; i< 5;i++){
-                        kern[i] = 2*pow(1-sqrt(zz[i]*zz[i]+r0*r0)/rr,3)*kfac;
                     }
-                    for(int i=0; i< 4;i++){
-                        //Trapezium rule. Factor of 1/2 goes away because we double the integral
-                        total +=(zz[i+1]-zz[i])*(kern[i+1]+kern[i]);
-                    }
-                    if(rr/2 > r0){
-                        double zz2[9]={0,zc/16.,zc/8.,zc/4.,3*zc/8,zc/2.,5/8.*zc,3*zc/4.,zc};
-                        double kern2[9];
-                        for(int i=0; i< 9;i++){
-                            double R = sqrt(zz2[i]*zz2[i]+r0*r0);
-                            kern2[i] = kfac*(1-6*(R/rr)*R/rr+6*pow(R/rr,3));
-                        }
-                        for(int i=0; i< 8;i++){
-                            //Trapezium rule. Factor of 1/2 goes away because we double the integral
-                            total +=(zz2[i+1]-zz2[i])*(kern2[i+1]+kern2[i]);
-                        }
-                    }
+                    sph_w[gy-lowgy][gx-lowgx]=compute_sph_cell_weight(rr,r0);
+                    total+=sph_w[gy-lowgy][gx-lowgx];
+                }
+            /*Then add the right fraction to the total array*/
+            for(int gy=lowgy;gy<=upgy;gy++)
+                for(int gx=lowgx;gx<=upgx;gx++){
 /*                     add_to_data_array(result, id1, id2, field, &il, gx, gy, val*total/weight); */
-                    *((double *) PyArray_GETPTR2(field,gx,gy))+=val*total/weight;
-/*                     field(gx,gy)+=val*total/weight; */
+                    *((double *) PyArray_GETPTR2(field,gx,gy))+=val*sph_w[gy-lowgy][gx-lowgx]/total/weight;
                 }
         }
         /*Empty on final iteration*/
