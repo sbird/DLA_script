ccc37190eff30aa72f7145e1746288598576fff2
Simeon Bird <spb@ias.edu>
2013-03-06 14:26:31 -0500

    Remove old not-working-any-more scipy.weave SPH interpolation method

--- fieldize.py
+++ fieldize.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """Methods for interpolating particle lists onto a grid. There are three classic methods:
     ngp - Nearest grid point (point interpolation)
     cic - Cloud in Cell (linear interpolation)
@@ -425,7 +426,6 @@ def cic_str(pos,value,field,in_radii,periodic=False):
     return field
 
 from _fieldize_priv import _SPH_Fieldize
-import sys
 
 def sph_str(pos,value,field,radii,weights=None,periodic=False):
     """Interpolate a particle onto the grid using an SPH kernel.
@@ -440,9 +440,7 @@ def sph_str(pos,value,field,radii,weights=None,periodic=False):
     if not check_input(pos,field):
         return field
 
-    nval=np.size(value)
     dim=np.shape(field)
-    nx = dim[0]
     dim=np.size(dim)
     if dim != 2:
         raise ValueError("Non 2D grid not supported!")
@@ -450,109 +448,8 @@ def sph_str(pos,value,field,radii,weights=None,periodic=False):
         weights = np.array([0])
 
     nval = _SPH_Fieldize(pos, radii, value, field, weights,periodic)
-    return field
-
-    expr="""for(int p=0;p<nval;p++){
-        //Temp variables
-        double pp[2];
-        pp[0]= pos(p,1);
-        pp[1]=pos(p,2);
-        double rr=radii(p);
-        double val= value(p);
-        double weight = 1;
-        if (weights(0) !=0)
-            weight=weights(p);
-        //99% of the kernel is inside 0.85 of the smoothing length.
-        //Neglect the rest.
-        int upgx = floor(pp[0]+0.85*rr);
-        int upgy = floor(pp[1]+0.85*rr);
-        int lowgx = floor(pp[0]-0.85*rr);
-        int lowgy = floor(pp[1]-0.85*rr);
-        //Try to save some integrations if this particle is totally in this cell
-        if (lowgx==upgx && lowgy==upgy && lowgx >= 0 && lowgy >= 0)
-                field(lowgx,lowgy)+=val/weight;
-        else{
-            //Deal with the edges
-            if(upgx > nx-1)
-                upgx=nx-1;
-            if(upgy > nx-1)
-                upgy=nx-1;
-            if(lowgx < 0)
-                lowgx=0;
-            if(lowgy < 0)
-                lowgy=0;
-            for(int gy=lowgy;gy<=upgy;gy++)
-                for(int gx=lowgx;gx<=upgx;gx++){
-                    double total=0;
-                    double xx = gx-pp[0]+0.5;
-                    double yy = gy-pp[1]+0.5;
-                    double r0 = sqrt(xx*xx+yy*yy);
-                    if(r0 > rr)
-                        continue;
-                    double h2 = rr*rr;
-                    //Do the z integration with the trapezium rule.
-                    //Evaluate this at some fixed (well-chosen) abcissae
-                    double zc=0;
-                    if(rr/2 > r0)
-                        zc=sqrt(h2/4-r0*r0);
-                    double zm = sqrt(h2-r0*r0);
-                    double zz[5]={zc,(3*zc+zm)/4.,(zc+zm)/2.,(zc+3*zm)/2,zm};
-                    double kern[5];
-                    double kfac= 8/M_PI/pow(rr,3);
-                    for(int i=0; i< 5;i++){
-                        kern[i] = 2*pow(1-sqrt(zz[i]*zz[i]+r0*r0)/rr,3)*kfac;
-                    }
-                    for(int i=0; i< 4;i++){
-                        //Trapezium rule. Factor of 1/2 goes away because we double the integral
-                        total +=(zz[i+1]-zz[i])*(kern[i+1]+kern[i]);
-                    }
-                    if(rr/2 > r0){
-                        double zz2[9]={0,zc/16.,zc/8.,zc/4.,3*zc/8,zc/2.,5/8.*zc,3*zc/4.,zc};
-                        double kern2[9];
-                        for(int i=0; i< 9;i++){
-                            double R = sqrt(zz2[i]*zz2[i]+r0*r0);
-                            kern2[i] = kfac*(1-6*(R/rr)*R/rr+6*pow(R/rr,3));
-                        }
-                        for(int i=0; i< 8;i++){
-                            //Trapezium rule. Factor of 1/2 goes away because we double the integral
-                            total +=(zz2[i+1]-zz2[i])*(kern2[i+1]+kern2[i]);
-                        }
-                    }
-                    field(gx,gy)+=val*total/weight;
-                }
-        }
-        }
-    """
-    try:
-        scipy.weave.inline(expr,['nval','pos','radii','value','field','nx','weights'],type_converters=scipy.weave.converters.blitz)
-    except Exception:
-	print "Falling back to slow!"
-        for p in xrange(0,nval):
-            #Upper and lower bounds
-            pp = pos[p,1:dim+1]
-            rr=radii[p]
-            val= value[p]
-            if weights[0] != 0:
-                weight = weights[p]
-            else:
-                weight = 1
-            #99% of the kernel is inside 0.85 of the smoothing length.
-            #Neglect the rest.
-            upg = np.array(np.floor(pp+0.85*rr),dtype=int)
-            lowg = np.array(np.floor(pp-0.85*rr),dtype=int)
-            #Try to save some integrations if this particle is totally in this cell
-            if (np.all(lowg == upg)):
-                    field[lowg[0],lowg[1]]+=val/weight
-            else:
-                #Deal with the edges
-                ind=np.where(upg > nx-1)
-                upg[ind]=nx-1
-                ind=np.where(lowg < 0)
-                lowg[ind]=0
-                #Corner values no longer need special attention because the sph kernel is just zero there
-                for gy in xrange(lowg[1],upg[1]+1):
-                    for gx in xrange(lowg[0],upg[0]+1):
-                        field[gx,gy]+=val*integrate_sph_kernel(rr,gx-pp[0],gy-pp[1])/weight
+    if nval < np.size(value):
+        raise ValueError("Something went wrong with interpolation")
     return field
 
 import scipy.integrate as integ
