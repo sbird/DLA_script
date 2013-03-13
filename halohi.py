Use the Rahmati fitting formula instead of the code until the code is fixed

halohi.py
@@ -217,7 +217,7 @@ class HaloHI:
             bar=f["PartType0"]
             ipos=np.array(bar["Coordinates"],dtype=np.float64)
             #Returns neutral density in atoms/cm^3 (physical)
-            irhoH0 = star.get_code_rhoHI(bar)
+            irhoH0 = star.get_reproc_rhoHI(bar)
             smooth = hsml.get_smooth_length(bar)
             # gas density in g/cm^3 (comoving)
             if not skip_grid == 2:

velhi.py
@@ -40,7 +40,7 @@ class VelocityHI(HaloHI):
             # Velocity in cm/s
             vel = np.array(bar["Velocities"],dtype=np.float64)*self.UnitVelocity_in_cm_per_s
             #We will weight by neutral mass per cell
-            irhoH0 = star.get_code_rhoHI(bar)
+            irhoH0 = star.get_reproc_rhoHI(bar)
             irho=np.array(bar["Density"],dtype=np.float64)*(self.UnitMass_in_g/self.UnitLength_in_cm**3)*self.hubble**2
             #HI * Cell Mass, internal units
             mass = np.array(bar["Masses"],dtype=np.float64)*irhoH0/irho