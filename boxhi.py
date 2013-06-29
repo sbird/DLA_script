Fix rho_HI plot: Noterdaeme multiplies by a factor of 0.76 for no real reason

boxhi.py
@@ -140,10 +140,11 @@ class BoxHI(HaloHI):
 
 
     def omega_DLA(self, thresh=20.3):
-        """Compute Omega_DLA, the sum of the mass in DLAs, divided by the critical density.
-            Ω_DLA = m_p * avg. column density / (1+z)^2 / length of column / rho_c
-            Note: If we want the neutral gas density rather than the neutral hydrogen density, divide by 0.76,
+        """Compute Omega_DLA, the sum of the neutral gas in DLAs, divided by the critical density.
+            Ω_DLA =  m_p * avg. column density / (1+z)^2 / length of column / rho_c / X_H
+            Note: If we want the neutral hydrogen density rather than the gas hydrogen density, multiply by 0.76,
             the hydrogen mass fraction.
+            The Noterdaeme results are GAS MASS
         """
         #Avg density in g/cm^3 (comoving) divided by critical density in g/cm^3
         omega_DLA=1000*self._rho_DLA(thresh)/self.rho_crit()
@@ -183,6 +184,23 @@ class BoxHI(HaloHI):
         pDLA = DLAs/size/self.absorption_distance()
         return pDLA
 
+    def line_density2(self,thresh=20.3):
+        """Compute the line density the other way, by summing the cddf. This is dN/dX = l_DLA(z)"""
+        (center,  fN) = self.column_density_function(minN=thresh,maxN=24)
+        NHI_table = 10**np.arange(thresh, 24, 0.2)
+        width =  np.array([NHI_table[i+1]-NHI_table[i] for i in range(0,np.size(NHI_table)-1)])
+        return np.sum(fN*width)
+
+    def omega_DLA2(self,thresh=20.3):
+        """Compute Omega_DLA the other way, by summing the cddf."""
+        (center,  fN) = self.column_density_function(minN=thresh,maxN=24)
+        #f_N* NHI is returned, in amu/cm^2/dX
+        NHI_table = 10**np.arange(thresh, 24, 0.2)
+        width =  np.array([NHI_table[i+1]-NHI_table[i] for i in range(0,np.size(NHI_table)-1)])
+        dXdcm = self.absorption_distance()/((self.box/self.nhalo)*self.UnitLength_in_cm/self.hubble)
+        return 1000*self.protonmass*np.sum(fN*center*width)*dXdcm/self.rho_crit()/(1+self.redshift)**2
+
+
     def find_cross_section(self, thresh=20.3):
         """Find the number of DLA cells nearest to each halo"""
         try:
@@ -212,8 +230,13 @@ class BoxHI(HaloHI):
             self.load_halo()
         self.halo_mass = np.zeros_like(self.sub_nHI_grid)
         celsz = 1.*self.box/self.ngrid[0]
+        #This is rho_c in units of h^-1 M_sun (kpc/h)^-3
+        rhom = 2.78e+11* self.omegam / (1e3**3)
+        #Mass of an SPH particle, in units of M_sun, x omega_m/ omega_b.
+        target_mass = self.box**3 * rhom / self.npart[0]
+        min_mass = target_mass * 400
         for nn in xrange(self.nhalo):
-            ind = np.where((self.real_sub_cofm[:,0] > nn*1.*self.box/self.nhalo)*(self.real_sub_cofm[:,0] < (nn+1)*1.*self.box/self.nhalo)*(self.real_sub_mass > 1e9))
+            ind = np.where((self.real_sub_cofm[:,0] > nn*1.*self.box/self.nhalo)*(self.real_sub_cofm[:,0] < (nn+1)*1.*self.box/self.nhalo)*(self.real_sub_mass > min_mass))
             cells = np.where(self.sub_nHI_grid[nn] > thresh)
             _find_halo_kernel(self.real_sub_cofm[ind],self.real_sub_mass[ind],cells[0], cells[1],self.halo_mass[nn], celsz)
 

dla_data.py
@@ -101,17 +101,29 @@ def dndx_pro(datadir="data"):
     """Plot the line densities for DLAs from Prochaska & Wolfe 2009, 0811.2003"""
     data = np.loadtxt(path.join(datadir,"dndx.txt"))
     zcen = (data[1:-1,0]+data[1:-1,1])/2.
-    plt.errorbar(zcen, data[1:-1,2],xerr=[zcen-data[1:-1,0], data[1:-1,1]-zcen], yerr=data[1:-1,3], fmt="s",color="black")
+    plt.errorbar(zcen, data[1:-1,2],xerr=[zcen-data[1:-1,0], data[1:-1,1]-zcen], yerr=data[1:-1,3], fmt="o",color="orange")
 
-def rhohi_pro(datadir="data"):
+def omegahi_pro(datadir="data"):
     """Plot the total rho_HI density for DLAs from Prochaska & Wolfe 2009, 0811.2003"""
     data = np.loadtxt(path.join(datadir,"dndx.txt"))
     zcen = (data[1:-1,0]+data[1:-1,1])/2.
-    plt.errorbar(zcen, data[1:-1,4],xerr=[zcen-data[1:-1,0], data[1:-1,1]-zcen], yerr=data[1:-1,5], fmt="s",color="black")
+    rhohi = data[1:-1,4]
+    #This is rho_crit at z=0
+    rho_crit = 9.3125685124148235e-30
+    #This converts from 1e8 M_sun/Mpc^3 to g/cm^3
+    conv = 6.7699111782945424e-33
+    #A factor of 0.76 from HI mass to gas mass
+    #Note: this factor is 0.74, so that the Noterdaeme
+    #Omega_DLA is numerically similar to the rho_HI of Prochaska
+    omega_DLA = rhohi*conv/rho_crit*1000
+    plt.errorbar(zcen, rhohi,xerr=[zcen-data[1:-1,0], data[1:-1,1]-zcen], yerr=data[1:-1,5], fmt="o",color="orange")
 
 def omegahi_not():
     """Omega_DLA from Noterdaeme 2012, 1210.1213"""
-    omega_dla = np.array([0.99, 0.87, 1.04, 1.1, 1.27])
+    #He divides these measurements by 0.76,
+    #which he thinks gives him the neutral gas mass in DLAs, because all this hydrogen
+    #is neutral. However, some of the hydrogen is molecular, so the factor is daft.
+    omega_dla = np.array([0.99, 0.87, 1.04, 1.1, 1.27])*0.76
     omega_err = np.array([0.05,0.04, 0.05,0.08,0.13])
     zz = [2.15,2.45,2.75,3.05,3.35]
     plt.errorbar(zz, omega_dla,xerr=0.15, yerr=omega_err, fmt="s",color="black")
@@ -120,10 +132,10 @@ def dndx_not():
     """dNdX from Noterdaeme 2012, 1210.1213"""
     #No error on dndz...use the systematic correction.
     dndz = np.array([0.2,0.2,0.25,0.29,0.36])
-    dndz_err = np.array([0.01,0.01,0.04,0.07,0.12])
+    #No error bars quoted in the paper for dndz (?)
     zz = [2.15,2.45,2.75,3.05,3.35]
     dzdx = np.array([3690/11625.,4509/14841.,2867/9900.,1620/5834.,789/2883.])
-    plt.errorbar(zz,dndz*dzdx,yerr=dndz_err,xerr=0.15,fmt="s",color="black")
+    plt.errorbar(zz,dndz*dzdx,xerr=0.15,fmt="s",color="black")
 
 # def prochaska_10_data():
 #     """Plot the six-power-law model of Prochaska 2010. A little too complicated."""