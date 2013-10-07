a6b4bf59337d36bc2c6a99ce115454de10ed95f8
Simeon Bird <spb@ias.edu>
2013-10-07 11:44:48 -0400

    Fix problem

--- halohi.py
+++ halohi.py
@@ -283,7 +283,6 @@ class HaloHI:
         #Larger numbers seem to be towards the beginning
         files.reverse()
         restart = 10
-        files = files[start:self.end]
         for xx in xrange(start, self.end):
             ff = files[xx]
             f = h5py.File(ff,"r")
