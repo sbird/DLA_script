9487374b004b9cab496e94962ee633747695c27f
Simeon Bird <spb@ias.edu>
2013-11-03 13:28:50 -0500

    Add a class which discards all interpolands not in particular locations

--- fieldize.h
+++ fieldize.h
@@ -1,7 +1,10 @@
 #include <Python.h>
 #include "numpy/arrayobject.h"
 #include <new>
+#include <map>
 
+//A class which interpolates data onto a regular grid, projecting
+//in the x direction and using SPH interpolation.
 class SphInterp
 {
     public:
@@ -27,11 +30,12 @@ class SphInterp
 
         inline void KahanSum(const double input, const int xoff, const int yoff)
         {
-          *(field+nx*xoff+yoff)+=input;
+            *(field+nx*xoff+yoff)+=input;
         }
 
 };
 
+//As above, but interpolation uses Kahan Summation
 class KahanSphInterp: public SphInterp
 {
     public:
@@ -51,15 +55,52 @@ class KahanSphInterp: public SphInterp
          *comp the compensation array, input the value to add this time.*/
         inline void KahanSum(const double input, const int xoff, const int yoff)
         {
-          const int off = nx*xoff+yoff;
-          const double yy = input - *(comp+off);
-          const double temp = *(field+off)+yy;     //Alas, field is big, y small, so low-order digits of y are lost.
-          *(comp+off) = (temp - *(field+off)) -yy; //(t - field) recovers the high-order part of y; subtracting y recovers -(low part of y)
-          *(field+off) = temp;               //Algebraically, c should always be zero. Beware eagerly optimising compilers!
+            const int off = nx*xoff+yoff;
+            const double yy = input - *(comp+off);
+            const double temp = *(field+off)+yy;     //Alas, field is big, y small, so low-order digits of y are lost.
+            *(comp+off) = (temp - *(field+off)) -yy; //(t - field) recovers the high-order part of y; subtracting y recovers -(low part of y)
+            *(field+off) = temp;               //Algebraically, c should always be zero. Beware eagerly optimising compilers!
         }
     private:
         double * comp;
 };
 
 
-/* class Discarding */
+//As above, but discard all interpolation except
+//onto a predefined list of array elements
+class DiscardingSphInterp: public SphInterp
+{
+    public:
+        DiscardingSphInterp(double * field_i, PyArrayObject * positions, int nx_i, bool periodic_i):
+            SphInterp(field_i, nx_i, periodic_i)
+        {
+            npy_intp nlist = PyArray_SIZE(positions);
+            //Build an index of the actual positions of each item we want in the output array.
+            for(int i=0; i<nlist;i++)
+                index.insert(std::pair<int,int>(*(int *)PyArray_GETPTR1(positions,i),i));
+            //Allocate Kahan compensation array, and throw if we can't.
+            comp = (double *) calloc(nlist,sizeof(double));
+            if( !comp )
+                throw std::bad_alloc();
+        }
+
+        /*Evaluate one iteration of Kahan Summation: sum is the current value of the field,
+         *comp the compensation array, input the value to add this time.*/
+        inline void KahanSum(const double input, const int xoff, const int yoff)
+        {
+            const int off = nx*xoff+yoff;
+            std::map<const int, const int>::iterator it = index.find(off);
+            if(it != index.end())
+            {
+                field[it->second]+= input;
+                const double yy = input - comp[it->second];
+                const double temp = field[it->second]+yy;     //Alas, field is big, y small, so low-order digits of y are lost.
+                comp[it->second] = temp - field[it->second] -yy; //(t - field) recovers the high-order part of y; subtracting y recovers -(low part of y)
+                field[it->second] = temp;               //Algebraically, c should always be zero. Beware eagerly optimising compilers!
+            }
+        }
+
+    private:
+        std::map<const int, const int> index;
+        double * comp;
+};
