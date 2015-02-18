7c44768cb89a3fa424e95ce033e1e2a8418868ee
Simeon Bird <sbird@andrew.cmu.edu>
2015-02-18 16:22:27 -0500

    Change CIV grid to plot around halos

--- make_civ_grid.py
+++ make_civ_grid.py
@@ -5,7 +5,8 @@ import halomet as hm
 from os.path import expanduser
 
 sim = "Cosmo"+str(sys.argv[2])+"_V6"
-base=expanduser("~/data/Cosmo/"+sim+"/L25n512/output/")
+box = sys.argv[3]
+base=expanduser("~/data/Cosmo/"+sim+"/L"+str(box)+"n512/output/")
 # base=expanduser("~/data/Cosmo/"+sim+"/L25n256/")
 
 snapnum=sys.argv[1]
@@ -14,10 +15,13 @@ outdir = base+"/snapdir_"+str(snapnum).rjust(3,'0')
 # 91,
 # 124,
 #]
-ahalo=hm.BoxCIV(base,snapnum, reload_file=True, nslice=30,savefile=outdir+"/boxciv_grid_low.hdf5",ngrid=5000)
-ahalo.column_density_function()
-ahalo.rho_DLA(13.)
-ahalo.line_density(13.)
-ahalo.omega_DLA(13.,15,1e8)
-print ahalo.omega_DLA(13.,15,1e8)
-ahalo.save_file(LLS_cut=11.5, DLA_cut = 13.)
+ahalo=hm.HaloCIV(base,snapnum, reload_file=True, minpart=1000,savefile=outdir+"/halociv_grid_low.hdf5")
+ahalo.save_file()
+ahalo=hm.HaloCIV(base,snapnum, ion=2,reload_file=True, minpart=1000,savefile=outdir+"/halocii_grid_low.hdf5")
+ahalo.save_file()
+#ahalo=hm.BoxCIV(base,snapnum, reload_file=False, nslice=30,savefile=outdir+"/boxciv_grid_low.hdf5",ngrid=5000)
+# ahalo.column_density_function()
+# ahalo.rho_DLA(13.)
+# ahalo.line_density(13.)
+# ahalo.omega_DLA(13.,15,1e8)
+#print ahalo.omega_DLA(13.,15,1e8)
